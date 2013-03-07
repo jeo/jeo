9f749297cbb335420eabdd16d587adce414a92c2
jdeolive <jdeolive@opengeo.org>
2013-03-06 21:08:46 -0700

    added javadoc for Disposable

--- core/src/main/java/org/jeo/data/Disposable.java
+++ core/src/main/java/org/jeo/data/Disposable.java
@@ -1,6 +1,21 @@
 package org.jeo.data;
 
+/**
+ * Interface for objects that should be disposed after usage.
+ * <p>
+ * Such objects typically hold onto resouces such as file handles, database connectionions, etc..
+ * </p>
+ * @author Justin Deoliveira, OpenGeo
+ *
+ */
 public interface Disposable {
 
+    /**
+     * Disposes the object. 
+     * <p>
+     * Application code must always be sure to call this method on any implementing class. 
+     * Implementing classes should handle multiple calls to this method.  
+     * </p>
+     */
     void dispose();
 }

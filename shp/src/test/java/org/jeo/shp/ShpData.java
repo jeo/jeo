42ff5ae06ce73261a39febd4f02ef503da40db25
jdeolive <jdeolive@opengeo.org>
2013-03-16 17:14:19 -0600

    added test data from SLD cookbook

--- shp/src/test/java/org/jeo/shp/ShpData.java
+++ shp/src/test/java/org/jeo/shp/ShpData.java
@@ -10,11 +10,36 @@ import java.io.IOException;
 public class ShpData {
 
     /**
-     * Returns the U.S. Cencus Berueau "states" shapefile file consisting of 49 states, excluding
-     * Alaska and Hawaii, including Distict of Columbia.
+     * A simple point shapefile, taken from the GeoServer SLD cookbook.
+     */
+    public static Shapefile point() throws IOException {
+        return shp("point");
+    }
+
+    /**
+     * A simple line shapefile, taken from the GeoServer SLD cookbook.
+     */
+    public static Shapefile line() throws IOException {
+        return shp("line");
+    }
+
+    /**
+     * A simple polygon shapefile, taken from the GeoServer SLD cookbook.
+     */
+    public static Shapefile poly() throws IOException {
+        return shp("polygon");
+    }
+
+    /**
+     * Returns the U.S. Census Bureau "states" shapefile file consisting of 49 states, excluding
+     * Alaska and Hawaii, including Distinct of Columbia.
      */
     public static Shapefile states() throws IOException {
-        File dir = unzip(ShpData.class.getResourceAsStream("states.shp.zip"), newTmpDir());
-        return new Shapefile(new File(dir, "states.shp"));
+        return shp("states");
+    }
+
+    static Shapefile shp(String name) throws IOException {
+        File dir = unzip(ShpData.class.getResourceAsStream(name + ".shp.zip"), newTmpDir());
+        return new Shapefile(new File(dir, name+".shp"));
     }
 }

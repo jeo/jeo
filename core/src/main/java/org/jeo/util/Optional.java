5a5ecf4f41a3e9adc5dd649fe2f462d09d030521
jdeolive <jdeolive@opengeo.org>
2013-05-15 21:49:30 -0600

    addd optional utility class

---
+++ core/src/main/java/org/jeo/util/Optional.java
@@ -0,0 +1,40 @@
+package org.jeo.util;
+
+/**
+ * Utility object for dealing with references that may be null.
+ * <p>
+ * This class is inspired by guava's Optional class.
+ * </p> 
+ * @author Justin Deoliveira, OpenGeo
+ */
+public class Optional<T> {
+
+    public static <X> Optional<X> of(X value) {
+        return new Optional<X>(value);
+    }
+
+    public static <X> Optional<X> nil(Class<X> clazz) {
+        return new Optional<X>(null);
+    }
+
+    T value;
+
+    public Optional(T value) {
+        this.value = value;
+    }
+
+    public boolean has() {
+        return value != null;
+    }
+
+    public T get() {
+        if (value == null) {
+            throw new IllegalArgumentException("null value");
+        }
+        return value;
+    }
+
+    public T or(T fallback) {
+        return value != null ? value : fallback;
+    }
+}

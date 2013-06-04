1e6df3fad25e566cc2c07bbbc36111d20dd6e890
jdeolive <jdeolive@opengeo.org>
2013-06-04 15:11:14 -0600

    javadocs for connection opts

--- format/postgis/src/main/java/org/jeo/postgis/PostGIS.java
+++ format/postgis/src/main/java/org/jeo/postgis/PostGIS.java
@@ -10,16 +10,35 @@ import org.jeo.util.Key;
 
 public class PostGIS implements Driver<PostGISWorkspace>{
 
+    /**
+     * Database key.
+     */
     public static final Key<String> DB = new Key<String>("db", String.class);
 
+    /**
+     * Host key, defaults to 'localhost'.
+     */
     public static final Key<String> HOST = new Key<String>("host", String.class, "localhost");
 
+    /**
+     * Port key, defaults to 5432.
+     */
     public static final Key<Integer> PORT = new Key<Integer>("port", Integer.class, 5432);
 
+    /**
+     * Host key, defaults to current user, obtained via <tt>System.getProperty("user.name")</tt>
+     */
     public static final Key<String> USER = 
             new Key<String>("user", String.class, System.getProperty("user.name"));
 
+    /**
+     * Password key. 
+     */
     public static final Key<String> PASSWD = new Key<String>("passwd", String.class);
+
+    public PostGISWorkspace open(PostGISOpts opts) throws IOException {
+        return new PostGISWorkspace(opts);
+    }
 
     @Override
     public String getName() {
@@ -49,10 +68,19 @@ public class PostGIS implements Driver<PostGISWorkspace>{
     @Override
     public PostGISWorkspace open(Map<?, Object> opts) throws IOException {
         PostGISOpts pgopts = new PostGISOpts(DB.get(opts));
-        pgopts.host(HOST.get(opts))
-          .port(PORT.get(opts))
-          .user(USER.get(opts))
-          .passwd(PASSWD.get(opts));
+
+        if (HOST.has(opts)) {
+            pgopts.host(HOST.get(opts));
+        }
+        if (PORT.has(opts)) {
+            pgopts.port(PORT.get(opts));
+        }
+        if (USER.has(opts)) {
+            pgopts.user(USER.get(opts));
+        }
+        if (PASSWD.has(opts)) {
+            pgopts.passwd(PASSWD.get(opts));
+        }
 
         return new PostGISWorkspace(pgopts);
     }

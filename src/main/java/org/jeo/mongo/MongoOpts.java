c1e4ada9924ed0f9fd3f536048c278339948407b
jdeolive <jdeolive@opengeo.org>
2013-07-17 11:33:19 -0600

    Changes to Registry and Workspace apis.
    - Added DataRef class
    - Changed Registry to contain both Dataset and Workspace
    - Modified Workspace.list() to return Iterable of DataRef

--- src/main/java/org/jeo/mongo/MongoOpts.java
+++ src/main/java/org/jeo/mongo/MongoOpts.java
@@ -1,6 +1,13 @@
 package org.jeo.mongo;
 
+import static org.jeo.mongo.MongoDB.*;
+
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.jeo.util.Key;
+import org.jeo.util.Password;
 
 import com.mongodb.DB;
 import com.mongodb.Mongo;
@@ -8,11 +15,20 @@ import com.mongodb.ServerAddress;
 
 public class MongoOpts {
     String db; 
-    String host = MongoDB.HOST.getDefault();
-    Integer port = MongoDB.PORT.getDefault();
-    String user = MongoDB.USER.getDefault();
-    String passwd;
-    
+    String host = HOST.getDefault();
+    Integer port = PORT.getDefault();
+    String user = USER.getDefault();
+    Password passwd;
+
+    public static MongoOpts fromMap(Map<?,Object> map) {
+        MongoOpts mopts = new MongoOpts(DB.get(map));
+        mopts.host(HOST.get(map))
+             .port(PORT.get(map))
+             .user(USER.get(map))
+             .passwd(PASSWD.get(map));
+        return mopts;
+    }
+
     public MongoOpts(String db) {
         this.db = db;
     }
@@ -32,7 +48,7 @@ public class MongoOpts {
         return this;
     }
     
-    public MongoOpts passwd(String passwd) {
+    public MongoOpts passwd(Password passwd) {
         this.passwd = passwd;
         return this;
     }
@@ -53,15 +69,27 @@ public class MongoOpts {
         return user;
     }
     
-    public String getPasswd() {
+    public Password getPasswd() {
         return passwd;
+    }
+
+    public Map<Key<?>,Object> toMap() {
+        Map<Key<?>,Object> map = new LinkedHashMap<Key<?>, Object>();
+        map.put(DB, db);
+        map.put(HOST, host);
+        map.put(PORT, port);
+        map.put(USER, user);
+        if (passwd != null) {
+            map.put(PASSWD, passwd);
+        }
+        return map;
     }
 
     public DB connect() throws IOException {
         ServerAddress server = new ServerAddress(host, port);
         Mongo mongo = new Mongo(server);
         DB database = mongo.getDB(db);
-        database.authenticate(user, passwd != null ? passwd.toCharArray() : new char[]{});
+        database.authenticate(user, passwd != null ? passwd.get(): new char[]{});
 
         return database;
     }

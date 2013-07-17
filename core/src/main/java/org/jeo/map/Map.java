fa28af5f433d43199f634df198a2bc40fd56cbb9
jdeolive <jdeolive@opengeo.org>
2013-07-17 08:34:05 -0600

    added Listener callback interface for Map

--- core/src/main/java/org/jeo/map/Map.java
+++ core/src/main/java/org/jeo/map/Map.java
@@ -1,11 +1,15 @@
 package org.jeo.map;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.jeo.data.Disposable;
 import org.jeo.proj.Proj;
 import org.osgeo.proj4j.CoordinateReferenceSystem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.vividsolutions.jts.geom.Envelope;
 
@@ -26,6 +30,50 @@ import com.vividsolutions.jts.geom.Envelope;
 public class Map implements Disposable {
 
     /**
+     * Listener callback.
+     */
+    public static interface Listener {
+
+        /**
+         * Callback fired when the bounds of the map are changed.
+         *  
+         * @param map The map.
+         * @param bounds The new bounds of the map.
+         * @param old The old bounds of the map, possibly <tt>null</tt>.
+         */
+        void onBoundsChanged(Map map, Envelope bounds, Envelope old);
+
+        /**
+         * Callback fired when the projection of the map are changed.
+         *  
+         * @param map The map.
+         * @param crs The new projection of the map.
+         * @param old The old projection of the map, possibly <tt>null</tt>.
+         */
+        void onCRSChanged(Map map, CoordinateReferenceSystem crs, CoordinateReferenceSystem old);
+
+        /**
+         * Callback fired when the dimensions of the map are changed.
+         *  
+         * @param map The map.
+         * @param width The new width of the map.
+         * @param height The new height of the map.
+         * @param oldWidth The old width of the map, possibly <tt>-1</tt>.
+         * @param oldHeight The old height of the map, possibly <tt>-1</tt>.
+         */
+        void onSizeChanged(Map map, int width, int height, int oldWidth, int oldHeight);
+
+        /**
+         * Callback fired when the style of the map is changed.
+         *  
+         * @param map The map.
+         * @param style The new style of the map.
+         * @param old The old style of the map, possibly <tt>null</tt>.
+         */
+        void onStyleChanged(Map map, Style style, Style old);
+    }
+
+    /**
      * default map width. 
      */
     public static int DEFAULT_WIDTH = 256;
@@ -42,6 +90,8 @@ public class Map implements Disposable {
         return new MapBuilder();
     }
 
+    static Logger LOG = LoggerFactory.getLogger(Map.class);
+
     int width = DEFAULT_WIDTH;
     int height = DEFAULT_HEIGHT;
 
@@ -53,6 +103,7 @@ public class Map implements Disposable {
     Style style = new Style();
 
     List<Disposable> cleanup = new ArrayList<Disposable>(); 
+    Set<Listener> callbacks = new LinkedHashSet<Listener>();
 
     /**
      * Creates a new empty map.
@@ -84,7 +135,9 @@ public class Map implements Disposable {
      * Sets the width of the map in "rendering" units, usually pixels. 
      */
     public void setWidth(int width) {
+        int oldWidth = this.width;
         this.width = width;
+        fireSizeChanged(oldWidth, height);
     }
 
     /**
@@ -98,7 +151,19 @@ public class Map implements Disposable {
      * Sets the height of the map in "rendering" units, usually pixels. 
      */
     public void setHeight(int height) {
+        int oldHeight = this.height;
         this.height = height;
+        fireSizeChanged(width, oldHeight);
+    }
+
+    public void setSize(int width, int height) {
+        int oldWidth = this.width;
+        int oldHeight = this.height;
+
+        this.width = width;
+        this.height = height;
+
+        fireSizeChanged(oldWidth, oldHeight);
     }
 
     /**
@@ -112,7 +177,9 @@ public class Map implements Disposable {
      * Sets the spatial/world extent of the map.
      */
     public void setBounds(Envelope bounds) {
+        Envelope oldBounds = this.bounds;
         this.bounds = bounds;
+        fireBoundsChanged(oldBounds);
     }
 
     /**
@@ -126,7 +193,9 @@ public class Map implements Disposable {
      * Sets the projection / coordinate reference system of the map.
      */
     public void setCRS(CoordinateReferenceSystem crs) {
+        CoordinateReferenceSystem oldCRS = this.crs;
         this.crs = crs;
+        fireCRSChanged(oldCRS);
     }
 
     /**
@@ -147,7 +216,9 @@ public class Map implements Disposable {
      * Sets the stylesheet containing the rules for rendering/symbolizing the map.  
      */
     public void setStyle(Style style) {
+        Style oldStyle = this.style;
         this.style = style;
+        fireStyleChanged(oldStyle);
     }
 
     /**
@@ -205,6 +276,68 @@ public class Map implements Disposable {
         return cleanup;
     }
 
+    /**
+     * Binds a callback to the map for listening to map property changed. 
+     * <p>
+     * The application should eventually call {@link #unbind(Listener)} although the 
+     * callback list is cleared when {@link #close()} is called. 
+     * </p>
+     */
+    public void bind(Listener callback) {
+        callbacks.add(callback);
+    }
+
+    /**
+     * Unbinds a previously bound callback from the map.
+     */
+    public void unbind(Listener callback) {
+        callbacks.remove(callback);
+    }
+
+    void fireBoundsChanged(Envelope old) {
+        for (Listener cb : callbacks) {
+            try {
+                cb.onBoundsChanged(this, bounds, old);
+            }
+            catch(Throwable t) {
+                LOG.debug("Callback failed", t.getMessage(), t);
+            }
+        }
+    }
+
+    void fireStyleChanged(Style old) {
+        for (Listener cb : callbacks) {
+            try {
+                cb.onStyleChanged(this, style, old);
+            }
+            catch(Throwable t) {
+                LOG.debug("Callback failed", t.getMessage(), t);
+            }
+        }
+    }
+
+    void fireSizeChanged(int oldWidth, int oldHeight) {
+        for (Listener cb : callbacks) {
+            try {
+                cb.onSizeChanged(this, width, height, oldWidth, oldHeight);
+            }
+            catch(Throwable t) {
+                LOG.debug("Callback failed", t.getMessage(), t);
+            }
+        }
+    }
+
+    void fireCRSChanged(CoordinateReferenceSystem old) {
+        for (Listener cb : callbacks) {
+            try {
+                cb.onCRSChanged(this, crs, old);
+            }
+            catch(Throwable t) {
+                LOG.debug("Callback failed", t.getMessage(), t);
+            }
+        }
+    }
+
     @Override
     public void close() {
         for (Disposable d : cleanup) {
@@ -212,5 +345,8 @@ public class Map implements Disposable {
         }
 
         layers.clear();
+        callbacks.clear();
     }
+
+    
 }

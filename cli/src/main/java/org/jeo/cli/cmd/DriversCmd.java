a3e25006d6848681d797662d0b15d9aa8b9b4023
jdeolive <jdeolive@boundlessgeo.com>
2013-10-15 16:33:31 -0600

    added optional argument to driver command for specific driver

--- cli/src/main/java/org/jeo/cli/cmd/DriversCmd.java
+++ cli/src/main/java/org/jeo/cli/cmd/DriversCmd.java
@@ -1,37 +1,87 @@
 package org.jeo.cli.cmd;
 
+import java.io.IOError;
+import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
 
 import org.jeo.cli.JeoCLI;
 import org.jeo.data.Driver;
 import org.jeo.data.Drivers;
 import org.jeo.geojson.GeoJSONWriter;
+import org.jeo.util.Key;
 
+import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 
 @Parameters(commandNames="drivers", commandDescription="Lists avialable format drivers")
 public class DriversCmd extends JeoCmd {
 
+    @Parameter(description="drivers", required=false)
+    List<String> drivers;
+
     @Override
     protected void doCommand(JeoCLI cli) throws Exception {
-        Iterator<Driver<?>> it = Drivers.list();
-
         GeoJSONWriter w = new GeoJSONWriter(cli.getConsole().getOutput(), 2);
-        w.array();
-        while(it.hasNext()) {
-            Driver<?> drv = it.next();
-            w.object();
 
-            w.key("name").value(drv.getName());
-             
-            w.key("aliases").array();
-            for (String a : drv.getAliases()) {
-                w.value(a);
+        if (drivers != null && !drivers.isEmpty()) {
+            if (drivers.size() > 1) {
+                w.array();
+            }
+
+            for (String driver : drivers) {
+                Driver<?> drv = Drivers.find(driver);
+                if (drv == null) {
+                    throw new IllegalArgumentException("No such driver: " + driver);
+                }
+
+                w.object();
+                printBrief(drv, w);
+
+                w.key("type").value(drv.getType().getSimpleName());
+
+                w.key("keys").object();
+                for (Key<?> key : drv.getKeys()) {
+                    w.key(key.getName()).object();
+                    w.key("type").value(key.getType().getSimpleName());
+                    if (key.getDefault() != null) {
+                        w.key("default").value(key.getDefault());
+                    }
+                    w.endObject();
+                }
+                w.endObject();
+
+                w.endObject();
+            }
+            if (drivers.size() > 1) {
+                w.endArray();
+            }
+        }
+        else {
+            Iterator<Driver<?>> it = Drivers.list();
+            
+            w.array();
+            while(it.hasNext()) {
+                Driver<?> drv = it.next();
+                
+                w.object();
+
+                printBrief(drv, w);
+                
+                w.endObject();
             }
             w.endArray();
-            w.endObject();
+        }
+
+    }
+
+    void printBrief(Driver<?> drv, GeoJSONWriter w) throws IOException {
+        w.key("name").value(drv.getName());
+        
+        w.key("aliases").array();
+        for (String a : drv.getAliases()) {
+            w.value(a);
         }
         w.endArray();
     }
-
 }

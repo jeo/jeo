f69ac66107dcb12017a5c3ffd2901ae604907e87
jdeolive <jdeolive@gmail.com>
2015-02-15 17:07:22 -0700

    Added protobuf module from contrib.

--- README.md
+++ README.md
@@ -2,8 +2,20 @@
 
 ## Building
 
-Building the jeo library requires [Apache Maven](http://maven.apache.org).
+Building jeo requires the following:
 
-    git clone https://github.com/jeo/jeo.git
-    cd jeo
+* [Java JDK](http://www.oracle.com/technetwork/java/javase/downloads/index.html) 1.7+
+* [Apache Maven ](http://maven.apache.org/download.cgi) 3.1+
+
+Once build pre-requisites are satisfied build with:
+
     mvn install
+
+### Protocol Buffers
+
+The library can be built with optional support for protocol buffers. This 
+requires: [Google Protocol Buffers](https://developers.google.com/protocol-buffers/docs/downloads) 2.6+. Once installed:
+
+    mvn -P protobuf install
+
+See the [protobuf README](format/protobuf) for more details.
---
+++ format/protobuf/src/main/java/org/jeo/protobuf/Protobuf.java
@@ -0,0 +1,65 @@
+package org.jeo.protobuf;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.jeo.data.FileDriver;
+import org.jeo.util.Messages;
+import org.jeo.vector.VectorDriver;
+import org.jeo.vector.Schema;
+
+/**
+ * Google Protocol Buffer (Protobuf) driver.
+ * <p>
+ * Usage:
+ * <pre><code>
+ * Protobuf.open(new File("states.pbf"));
+ * </code></pre>
+ * </p>
+ * 
+ * @author Justin Deoliveira, OpenGeo
+ */
+public class Protobuf extends FileDriver<ProtobufDataset> implements VectorDriver<ProtobufDataset> {
+
+    public static ProtobufDataset open(File file) throws IOException {
+        return new ProtobufDataset(file);
+    }
+
+    @Override
+    public String getName() {
+        return "Protobuf";
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return Arrays.asList("pb", "pbf");
+    }
+
+    @Override
+    public Class<ProtobufDataset> getType() {
+        return ProtobufDataset.class;
+    }
+    
+    @Override
+    public ProtobufDataset open(File file, Map opts) throws IOException {
+        return open(file);
+    }
+
+    @Override
+    public boolean supports(Capability cap) {
+        return false;
+    }
+
+    @Override
+    public boolean canCreate(Map<?, Object> opts, Messages msgs) {
+        return FILE.in(opts);
+    }
+
+    @Override
+    public ProtobufDataset create(Map<?, Object> opts, Schema schema) throws IOException {
+        return new ProtobufDataset(FILE.get(opts), schema);
+    }
+}

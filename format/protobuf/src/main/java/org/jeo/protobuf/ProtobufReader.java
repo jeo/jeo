cabea984ecd8a8dfb7837477b995e09cc1b92962
jdeolive <jdeolive@gmail.com>
2015-02-16 17:49:49 -0700

    Copyright/license headers for protobuf module, some javadocs and cleanup.

--- format/protobuf/src/main/java/org/jeo/protobuf/ProtobufReader.java
+++ format/protobuf/src/main/java/org/jeo/protobuf/ProtobufReader.java
@@ -1,4 +1,18 @@
-package org.jeo.protobuf;
+
+/* Copyright 2015 The jeo project. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */package org.jeo.protobuf;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -27,6 +41,9 @@ import com.vividsolutions.jts.geom.CoordinateSequence;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LinearRing;
 
+/**
+ * Reads a protocol buffer stream.
+ */
 public class ProtobufReader {
 
     static GeometryFactory gf = new GeometryFactory();
@@ -107,6 +124,14 @@ public class ProtobufReader {
         };
     }
 
+    /**
+     * Forces the reader to read until the last feature in the stream.
+     * <p>
+     * By default the reader will read until no more bytes are available in the input stream.
+     * When this flag is set the reader will read until the feature marked as last in the stream
+     * has been encountered.
+     * </p>
+     */
     public ProtobufReader setReadUntilLastFeature() {
         end = new End() {
             @Override
--- format/protobuf/src/main/java/org/jeo/protobuf/ProtobufWriter.java
+++ format/protobuf/src/main/java/org/jeo/protobuf/ProtobufWriter.java
@@ -1,3 +1,17 @@
+/* Copyright 2015 The jeo project. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jeo.protobuf;
 
 import java.io.IOException;
@@ -28,6 +42,9 @@ import com.vividsolutions.jts.geom.CoordinateSequence;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Writes out a protocol buffer stream.
+ */
 public class ProtobufWriter implements Disposable {
 
     static Logger LOG = LoggerFactory.getLogger(ProtobufWriter.class);

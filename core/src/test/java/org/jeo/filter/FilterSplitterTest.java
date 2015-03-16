367b8132b7fbc688b7ae15eace298e9e84444311
jdeolive <jdeolive@gmail.com>
2015-03-15 21:33:27 -0700

    Added FilterSplitter class.
    Splits filters into two based on pass/fail of criteria speciifed as a visitor.

---
+++ core/src/test/java/org/jeo/filter/FilterSplitterTest.java
@@ -0,0 +1,92 @@
+package org.jeo.filter;
+
+import org.jeo.filter.cql.CQL;
+import org.jeo.util.Pair;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FilterSplitterTest {
+
+    @Test
+    public void testFlat() throws Exception {
+        Filter f = CQL.parse("foo = TRUE AND x = TRUE");
+        Pair<Filter,Filter> split = split(f);
+        assertEquals(f, split.first);
+        assertEquals(Filters.all(), split.second);
+
+        f = CQL.parse("foo = TRUE AND x = FALSE");
+        split = split(f);
+        assertEquals(CQL.parse("foo = TRUE"), split.first);
+        assertEquals(CQL.parse("x = FALSE"), split.second);
+
+        f = CQL.parse("foo = FALSE AND x = FALSE");
+        split = split(f);
+        assertEquals(Filters.all(), split.first);
+        assertEquals(f, split.second);
+
+        f = CQL.parse("foo = TRUE OR x = TRUE");
+        split = split(f);
+        assertEquals(f, split.first);
+        assertEquals(Filters.all(), split.second);
+
+        f = CQL.parse("foo = TRUE OR x = FALSE");
+        split = split(f);
+        assertEquals(Filters.all(), split.first);
+        assertEquals(f, split.second);
+
+        f = CQL.parse("foo = FALSE OR x = FALSE");
+        split = split(f);
+        assertEquals(Filters.all(), split.first);
+        assertEquals(f, split.second);
+    }
+
+    @Test
+    public void testNested() throws Exception {
+        Filter f = CQL.parse("(foo = TRUE AND x = TRUE) AND z = TRUE");
+        Pair<Filter,Filter> split = split(f);
+        assertEquals(f, split.first);
+        assertEquals(Filters.all(), split.second);
+
+        f = CQL.parse("(foo = TRUE OR x = FALSE) AND z = TRUE");
+        split = split(f);
+        assertEquals(CQL.parse("z = TRUE"), split.first);
+        assertEquals(CQL.parse("foo = TRUE OR x = FALSE"), split.second);
+
+        f = CQL.parse("(foo = TRUE AND x = FALSE) AND z = TRUE");
+        split = split(f);
+        assertEquals(CQL.parse("foo = TRUE AND z = TRUE"), split.first);
+        assertEquals(CQL.parse("x = FALSE"), split.second);
+
+        f = CQL.parse("(foo = TRUE AND x = FALSE) OR z = TRUE");
+        split = split(f);
+        assertEquals(Filters.all(), split.first);
+        assertEquals(CQL.parse("(foo = TRUE AND x = FALSE) OR z = TRUE"), split.second);
+
+    }
+
+    @Test
+    public void testNonLogic() throws Exception {
+        Filter f = CQL.parse("foo = TRUE");
+        Pair<Filter,Filter> split = split(f);
+        assertEquals(f, split.first);
+        assertEquals(Filters.all(), split.second);
+
+        f = CQL.parse("foo = FALSE");
+        split = split(f);
+        assertEquals(Filters.all(), split.first);
+        assertEquals(f, split.second);
+    }
+
+
+    Pair<Filter,Filter> split(Filter f) {
+        FilterSplitter splitter = new FilterSplitter(new FilterWalker<Boolean>() {
+            @Override
+            public Boolean visit(Comparison<?> compare, Object obj) {
+                String s = compare.right().evaluate(null).toString();
+                return Boolean.valueOf(s);
+            }
+        });
+        return splitter.split(f);
+    }
+}

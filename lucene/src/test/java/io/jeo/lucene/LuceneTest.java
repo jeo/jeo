406202a46b00ab5e7b32cd52d57a55638fa44a9d
jdeolive <jdeolive@gmail.com>
2015-06-12 15:31:22 -0600

    Updating package namespace for lucene module.

--- lucene/src/test/java/io/jeo/lucene/LuceneTest.java
+++ lucene/src/test/java/io/jeo/lucene/LuceneTest.java
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jeo.lucene;
+package io.jeo.lucene;
 
 import com.vividsolutions.jts.geom.Polygonal;
 import io.jeo.vector.Feature;

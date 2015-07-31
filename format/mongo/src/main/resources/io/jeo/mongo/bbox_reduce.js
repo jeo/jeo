b199168036bf6a78f56fb25998f2697d028a24f5
jdeolive <jdeolive@gmail.com>
2015-07-30 21:08:23 -0600

    Moving mongo module to contrib

--- format/mongo/src/main/resources/io/jeo/mongo/bbox_reduce.js
+++
@@ -1,8 +0,0 @@
-function(a, boxes) {
-  var x1 = Math.min.apply(Math, boxes.map(function(box){ return box[0]; }));
-  var y1 = Math.min.apply(Math, boxes.map(function(box){ return box[1]; }));
-  var x2 = Math.max.apply(Math, boxes.map(function(box){ return box[2]; }));
-  var y2 = Math.max.apply(Math, boxes.map(function(box){ return box[3]; }));
-
-  return {x1:x1, y1:y1, x2:x2, y2:y2};
-}
